6b6c0f62d7fcfd6b97c9854f539ed1dd7e262412
cschad01 <cschad01@tufts.edu>
2026-08-05 19:52:54 -0400

    Update appTwo.js

--- appTwo.js
+++ appTwo.js
@@ -1,5 +1,13 @@
 const http = require("http");
 const url = require("url");
+const dns = require("node:dns");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const fs = require("fs");
+const readline = require("readline");
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = "mongodb+srv://cschad01_db_user:0Y1OTOiX4ERfdpFg@cluster0.ujctirb.mongodb.net/?retryWrites=true&w=majority";
 
 var port = process.env.PORT || 3000;
 // var port = 8080;   // Uncomment if running locally
@@ -36,23 +44,68 @@ http.createServer(function(req, res) {
     // Process page
     else if (req.url.startsWith("/process")) {
 
-        let query = url.parse(req.url, true).query;
-        let search = query.search;
+    let query = url.parse(req.url, true).query;
+    let search = query.search;
 
-        console.log("User entered: " + search);
+    console.log("User entered: " + search);
 
-        // see if the first character the user entered is a number
+    MongoClient.connect(mongoURL, function(err, db) {
+
+        if (err) {
+            console.log("Connection error: " + err);
+            res.end("Database connection error");
+            return;
+        }
+
+        console.log("Connected to MongoDB");
+
+       let dbo = db.db("MassZips");
+       let collection = dbo.collection("places");
+       let theQuery;
+
+    //    checl to see if the first character is a number
+    // if it is then its a zip code
         if (!isNaN(search.charAt(0))) {
             console.log("Searching by ZIP code");
+            theQuery = { zips: search };
         }
+        // serachign by place
         else {
             console.log("Searching by place");
+            theQuery = { place: search };
         }
 
-        res.write("<h2>Search received.</h2>");
-        res.write("<p>Check the console for the results.</p>");
-        res.end();
-    }
+        collection.find(theQuery).toArray(function(err, items) {
+
+            if (err) {
+                console.log("Search error: " + err);
+                res.write("<h2>Database search error</h2>");
+            }
+
+            // if nothing is returned then nothing was found
+            else if (items.length == 0) {
+                console.log("No matching place or ZIP code was found.");
+                res.write("<h2>No match found</h2>");
+            }
+            else {
+                // print out and log the place with the associated zip codes.
+                for (let i = 0; i < items.length; i++) {
+                    console.log("Place: " + items[i].place);
+                    console.log("ZIP codes: " + items[i].zips.join(", "));
+
+                    res.write("<h2>" + items[i].place + "</h2>");
+                    res.write(
+                        "<p>ZIP Codes: " +
+                        items[i].zips.join(", ") +
+                        "</p>"
+                    );
+                }
+            }
+            res.end();
+            db.close();
+        });
+    });
+}
 
 }).listen(port);
 
